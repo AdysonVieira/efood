c4dc353a713553b382cc5fa89f9770b94cb4964f
Adyson Vieira <contato.adyson@gmail.com>
2024-04-30 10:45:33 -0300

    feat: add Header component to Home

---
+++ app/_components/Header.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+
+const Header = () => {
+  return (
+    <header className="flex justify-between px-5 pt-6">
+      <Image src={"/efoodlogo.png"} alt="Efood logo" height={40} width={68} />
+      <Button variant="ghost">
+        <MenuIcon size={20} />
+      </Button>
+    </header>
+  );
+};
+
+export default Header;
