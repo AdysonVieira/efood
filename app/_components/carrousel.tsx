24851f11b31efc8658dc84007b90a17e1fca5850
Adyson Vieira <contato.adyson@gmail.com>
2024-05-01 21:10:13 -0300

    feat: add carrousel component

---
+++ app/_components/carrousel.tsx
@@ -0,0 +1,49 @@
+import { Product } from "@prisma/client";
+import { db } from "../_lib/prisma";
+import ProductCard from "./product-card";
+import Link from "next/link";
+import { ChevronRight } from "lucide-react";
+
+interface CarrouselProps {
+  title: string;
+}
+
+const Carrousel = async ({ title }: CarrouselProps) => {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    take: 10,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
+  return (
+    <div className="px-5">
+      <div className="flex justify-between pb-5">
+        <h2 className="text-lg font-bold">{title}</h2>
+        <Link href="" className="flex items-center">
+          <span role="a" className="text-primary">
+            Ver mais
+          </span>
+          <ChevronRight size={14} className="text-primary" />
+        </Link>
+      </div>
+
+      <ul className="flex gap-5 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Carrousel;
---
+++ app/_components/product-card.tsx
@@ -0,0 +1,58 @@
+import { Prisma } from "@prisma/client";
+import Image from "next/image";
+import { Badge } from "./ui/badge";
+import { ArrowDown } from "lucide-react";
+import { calculateProductDiscount } from "../_helpers/calculate-product-discount";
+import { formatCurrency } from "../_helpers/format-currency";
+
+interface ProductCardProps {
+  product: Prisma.ProductGetPayload<{
+    include: {
+      restaurant: {
+        select: {
+          name: true;
+        };
+      };
+    };
+  }>;
+}
+const ProductCard = ({ product }: ProductCardProps) => {
+  return (
+    <li className="max-w-[150px]">
+      {/* Card image */}
+      <div className="relative h-[150px] w-[150px] overflow-hidden rounded-sm">
+        <Image
+          src={product.imageUrl}
+          alt={product.description}
+          width={150}
+          height={150}
+          className=" aspect-square rounded-sm object-cover"
+        />
+        <Badge className="absolute left-2 top-2">
+          <ArrowDown size={10} />
+          {`${product.discountPercentage}%`}
+        </Badge>
+      </div>
+
+      {/* Card informations */}
+      <div className="w-[140px]">
+        <h2 className="truncate">{product.name}</h2>
+        <div className="flex w-full items-end gap-2">
+          <h3 className="font-bold">
+            {formatCurrency(calculateProductDiscount(product))}
+          </h3>
+          {product.discountPercentage > 0 && (
+            <span className="text-sm text-muted-foreground line-through">
+              {formatCurrency(Number(product.price))}
+            </span>
+          )}
+        </div>
+        <span className="truncate text-sm text-muted-foreground">
+          {product.restaurant.name}
+        </span>
+      </div>
+    </li>
+  );
+};
+
+export default ProductCard;
